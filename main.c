d6a6c55d2ca57a1bb0d2ecbf5cb01723c0a65b7a
rubendaluz <rubendaluz684@gmail.com>
2022-05-20 10:51:20 +0100

    versão 1.1

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 //maximo de culturas por propriedade
 #define MAX_CT 20 
 
+//Declaração das estruturas
 typedef struct utilizador{
     char username[20];
     char password[20];
@@ -27,10 +28,48 @@ typedef struct propriedade{
 
 }PROPRIEDADE;
 
+//Funcões e procedimentos
+int menu(){
+    int opcao;
+
+    printf("MENU:\n");
+    //printf("");
+    //printf("");
+    scanf(" %d", &opcao); //Leitura da opção do utilizaddor
+
+    return opcao;
+}
+
 
 int main () {
- 
+    
+    //Declaração das variaveis
+    FILE *file;
+    int op; //Coleta a opção
+    
+    
+    
+    //Abertura e verificação do ficheiro binário
+    if((file = fopen("dado.dat", "a+b")) == NULL){
+        printf("Erro na abertura do ficheiro binario!!!\n");
+        exit(EXIT_FAILURE);
+    }
 
+    //Loop do Menu
+    while (1)
+    op = menu();
+    {
+        switch (op)
+        {
+        case 1:
+            break;
+    
+        default:
+            printf("Opcao invalida!!\n");
+            break;
+        }
+    }
+    
 
 
     return 0;
